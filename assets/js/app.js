f6052e290c6e734ca603884fced4f68d64d16d56
mikakorkiakoski <mika.korkiakoski@icloud.com>
2023-10-08 00:28:41 +0300

    Added Contact-Form for Message

--- assets/js/app.js
+++ assets/js/app.js
@@ -79,5 +79,20 @@ document.addEventListener("click", function(event) {
 
 //================== SideMenu For Media Screens max 600px End ================== //
 
+//================== Contact Form Start ======================= //
+
+const scriptURL = 'https://script.google.com/macros/s/AKfycbxbREj0hFOBjyXJSGiDwuWHA791WliA8Z3CIh39i8ue-R3hsIqrH71DsFLPzxHaM7Zy/exec'
+const form = document.forms['submit-to-google-sheet']
+
+form.addEventListener("submit", e => {
+  e.preventDefault()
+  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+    .then(response => console.log('Success!', response))
+    .catch(error => console.error('Error!', error.message))
+})
+
+//================== Contact Form End ======================= //
+
+
 
 
